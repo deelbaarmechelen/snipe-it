078dbc04d9a955fb90757a6a5c342fc51c1e452f
Bernard Butaye <bernardenmartine@gmail.com>
2020-01-26 22:59:24 +0100

    add post deploy cache clearing actions

--- .rocketeer/hooks.php
+++ .rocketeer/hooks.php
@@ -24,7 +24,15 @@ return [
     // Tasks to execute after the core Rocketeer Tasks
     'after'  => [
         'setup'   => [],
-        'deploy'  => ['cp vendor/renardeau/klusbib-module/public/img/DBM_avatar_* ../../shared/public/uploads/avatars'],
+        'deploy'  => array (
+            'cp vendor/renardeau/klusbib-module/public/img/DBM_avatar_* ../../shared/public/uploads/avatars',
+            'php artisan migrate',
+            'php artisan config:clear',
+            'php artisan config:cache',
+            'php artisan view:clear',
+            'php artisan cache:clear',
+            'php artisan route:clear',
+            ),
         'cleanup' => [],
     ],
 
