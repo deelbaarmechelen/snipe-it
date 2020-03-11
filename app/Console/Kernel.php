6931c62da18c244d72bfda1936e01fc83d17d017
Bernard Butaye <bernardenmartine@gmail.com>
2020-03-11 22:14:29 +0100

    sync-lendings dry run

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,38 +10,17 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    // KB-MAN - start
     /**
      * The Artisan commands provided by your application.
      *
      * @var array
      */
-     /* obsolete??? Seems no longer present in latest snipeit release 4.8.0
     protected $commands = [
-        Commands\PaveIt::class,
-        Commands\CreateAdmin::class,
-        Commands\SendExpirationAlerts::class,
-        Commands\SendInventoryAlerts::class,
-        Commands\SendExpectedCheckinAlerts::class,
-        Commands\ObjectImportCommand::class,
-        Commands\Version::class,
-        Commands\SystemBackup::class,
-        Commands\DisableLDAP::class,
-        Commands\Purge::class,
-        Commands\LdapSync::class,
-        Commands\FixDoubleEscape::class,
-        Commands\RecryptFromMcrypt::class,
-        Commands\ResetDemoSettings::class,
-        Commands\SyncAssetLocations::class,
-        Commands\RegenerateAssetTags::class,
-        Commands\SyncAssetCounters::class,
-        Commands\RestoreDeletedUsers::class,
-        Commands\SendUpcomingAuditReport::class,
-        Commands\ImportLocations::class,
-        // KB-MAN - start
-        \Modules\Klusbib\Console\UpdateAvatar::class
-        // KB-MAN - end
-    ];*/
-
+        \Modules\Klusbib\Console\Commands\SyncLendings::class
+    ];
+    // KB-MAN - end
 
     /**
      * Define the application's command schedule.
@@ -58,9 +37,6 @@ class Kernel extends ConsoleKernel
         $schedule->command('snipeit:backup')->weekly();
         $schedule->command('backup:clean')->daily();
         $schedule->command('snipeit:upcoming-audits')->daily();
-        // KB-MAN - start
-//        $schedule->command('klusbib:update-avatar')->daily();
-        // KB-MAN - end
     }
 
     protected function commands()
