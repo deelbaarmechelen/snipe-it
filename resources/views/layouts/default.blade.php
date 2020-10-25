3d46331715f1ad80db1380452ce5d6601b3156e6
Bernard Butaye <renardeau@users.noreply.github.com>
2020-10-25 21:27:58 +0100

    update klusbib-module + extract klusbib menu in separate module file

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -101,6 +101,13 @@
             <script src="{{ url(asset('js/respond.js')) }}" nonce="{{ csrf_token() }}"></script>
        @endif
        <![endif]-->
+
+      <!-- KB-MAN start-->
+      {{--            @if (\Nwidart\Modules\Module::getLaravel()::has('klusbib'))--}}
+      @stack('custom-scripts')
+      {{--@endif--}}
+      <!-- KB-MAN end -->
+
   </head>
   <body class="sidebar-mini skin-blue {{ (session('menu_state')!='open') ? 'sidebar-mini sidebar-collapse' : ''  }}">
     <div class="wrapper">
@@ -705,82 +712,11 @@
             @endcan
 
           <!-- KB-MAN start-->
-            {{--@can('index', \App\Models\Asset::class)--}}
-            {{--<li class="treeview{{ (Request::is('hardware*') ? ' active' : '') }}">--}}
-{{--            @if (\Nwidart\Modules\Module::getLaravel()::has('klusbib'))--}}
-            <li class="treeview active }}">
-                <a href="#"><i class="fa fa-wrench"></i>
-                    <span>{{ trans('klusbib::general.name') }}</span>
-                    <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                    {{--<li>--}}
-                        {{--<a href="{{ url('klusbib') }}">--}}
-                            {{--{{ trans('general.list_all') }}--}}
-                        {{--</a>--}}
-                    {{--</li>--}}
-                    @if (\Illuminate\Support\Facades\Route::has('klusbib.home'))
-                    <li>
-                        <a href="{{ route('klusbib.home') }}">
-                            <i class="fa fa-dashboard"></i>
-                            <span>Dashboard</span>
-                        </a>
-                    </li>
-                    @endif
-                    @if (\Illuminate\Support\Facades\Route::has('klusbib.users.index'))
-                    <li>
-                        <a href="{{ route('klusbib.users.index') }}">
-                            <i class="fa fa-users"></i>
-                            {{ trans('general.people') }}
-                        </a>
-                    </li>
-                    @endif
-                    @if (\Illuminate\Support\Facades\Route::has('klusbib.reservations.index'))
-                        <li>
-                            <a href="{{ route('klusbib.reservations.index') }}">
-                                <i class="fa fa-calendar"></i>
-                                <span>Reservaties</span>
-                            </a>
-                        </li>
-                    @endif
-                    {{--@if (\Illuminate\Support\Facades\Route::has('klusbib.deliveries.index'))--}}
-                        {{--<li>--}}
-                            {{--<a href="{{ route('klusbib.deliveries.index') }}">--}}
-                                {{--<i class="fa fa-bicycle"></i>--}}
-                                {{--<span>Deliveries</span>--}}
-                            {{--</a>--}}
-                        {{--</li>--}}
-                    {{--@endif--}}
-                    @if (\Illuminate\Support\Facades\Route::has('klusbib.memberships.index'))
-                        <li>
-                            <a href="{{ route('klusbib.memberships.index') }}">
-                                <i class="fa fa-id-card"></i>
-                                <span>Lidmaatschappen</span>
-                            </a>
-                        </li>
-                    @endif
-                    @if (\Illuminate\Support\Facades\Route::has('klusbib.payments.index'))
-                        <li>
-                            <a href="{{ route('klusbib.payments.index') }}">
-                                <i class="fa fa-money"></i>
-                                <span>Betalingen</span>
-                            </a>
-                        </li>
-                    @endif
-                    @if (\Illuminate\Support\Facades\Route::has('klusbib.lendings.index'))
-                        <li>
-                            <a href="{{ route('klusbib.lendings.index') }}">
-                                <i class="fa"></i>
-                                <span>Ontleningen</span>
-                            </a>
-                        </li>
-                    @endif
-                </ul>
-            </li>
+            {{--            @if (\Nwidart\Modules\Module::getLaravel()::has('klusbib'))--}}
+            @include ('klusbib::partials.menu')
+            @yield('klusbib-menu', '')
             {{--@endif--}}
-            {{--@endcan--}}
-
-        <!-- KB-MAN end -->
+          <!-- KB-MAN end -->
           </ul>
         </section>
         <!-- /.sidebar -->
